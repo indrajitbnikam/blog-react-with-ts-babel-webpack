3911c5bb56820da5b2fae03ac5641384728265f5
Indrajeet Nikam <indrajeet.nikam@cashfree.com>
2021-09-12 17:12:56 +0530

    add webpack.config.js docs

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,38 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.tsx"),
+  entry: path.resolve(
+    __dirname,
+    "./src/index.tsx"
+  ) /* entry-point(s) to start app bundling process */,
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
-  },
+  } /* configure how modules are resolved */,
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
-      },
+      } /* rules to process .ts, .tsx, .js, .jsx files */,
       {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
-      },
+      } /* rules to process .scss, .sass files */,
     ],
-  },
+  } /* determines how different module types will be treated */,
   output: {
     path: path.resolve(__dirname, "./build"),
     filename: "bundle.js",
-  },
-  mode: "development",
-  devtool: "cheap-module-source-map",
+  } /* where to output your bundle */,
+  mode: "development" /* for built-in optimizations -> 'none' | 'development' | 'production' */,
+  devtool: "cheap-module-source-map" /* how source maps are generated */,
   devServer: {
-    open: true,
-  },
+    open: true, // opens the app when you run "npm start"
+  } /* options for webpack-dev-server */,
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "./src/index.html"),
-    }),
+    }) /* simplifies the creation of HTML files to serve your webpack bundles */,
   ],
 };
